b2b05f2164c1495c8306305521a06e0576fa4b09
Jason Walsh <jwalsh@saylent.com>
2017-08-25 21:00:21 -0400

    Chaining example

---
+++ README.md
@@ -0,0 +1,54 @@
+# Background 
+
+This is a series of exercises to implement some core JavaScript libraries from the ground up. 
+
+## 2006: jQuery 
+
+http://ejohn.org/blog/annotated-version-of-the-original-jquery-release/
+
+- Sizzle 
+- NodeList 
+- trigger()
+- on()
+- ajax()
+
+## 2006: YUI
+
+https://en.wikipedia.org/wiki/YUI_Library
+
+### 2007: env.js 
+
+http://ejohn.org/blog/bringing-the-browser-to-the-server/
+
+## 2009: Underscore 
+
+## 2009: Protovis / D3
+
+## 2010: Backbone
+
+http://backbonejs.org/docs/backbone.html
+
+- Model 
+- Collection 
+
+## 2011: TODOMVC
+
+http://todomvc.com/
+https://github.com/tastejs/todomvc
+
+## 2011: ADSafe 
+
+http://www.adsafe.org/
+https://github.com/douglascrockford/ADsafe
+
+## 2013: Bluebird
+
+https://en.wikipedia.org/wiki/Futures_and_promises
+
+https://github.com/Reactive-Extensions/RxJS
+
+
+## 2015: RxJS
+
+http://reactivex.io/rxjs/
+https://github.com/Reactive-Extensions/RxJS
--- chaining/index.js
+++ chaining/index.js
@@ -1,40 +1,78 @@
 let o = a => {
   let scaffold = {
+
     a: f => {
       console.log('a');
-
+      // f is an operation
+      if (typeof f === 'function' && typeof a === 'object') {
+        let op = f;
+        let result = [];
+        for (var i = 0; i < a.length; i++) {
+          result.push(op(a[i]));
+        }
+        // console.log(a, result);
+        a = result;
+      }
       return scaffold;
     },
+
     b: f => {
       console.log('b');
-      // f is now a predicate
-      let pred = f;
-      let base = [];
-      for (var i = 0; i < a.length; i++) {
-        if (pred(a[i])) {
-          base.push(a[i]);
+      // f is a predicate
+      if (typeof f === 'function' && typeof a === 'object') {
+        let pred = f;
+        let result = [];
+        for (var i = 0; i < a.length; i++) {
+          if (pred(a[i])) {
+            result.push(a[i]);
+          }
         }
+        // console.log(a, result);
+        a = result;
       }
-      console.log(a, base);
-      a = base;
       return scaffold;
     },
-    c: f => { console.log('c'); return scaffold; },
-    d: f => { console.log('d'); return a; }
+
+    c: (f, acc) => {
+      console.log('c');
+      // f is a reducer with an accumulator
+      if (typeof f === 'function' && typeof a === 'object' && typeof acc !== 'undefined') {
+        let result = acc;
+        let reducer = f;
+        for (var i = 0; i < a.length; i++) {
+          result = reducer(result, a[i]);
+        }
+        // console.log(a, result);
+        a = result;
+      }
+      return scaffold;
+    },
+
+    d: f => {
+      console.log('d');
+      return a;
+    }
   };
   return scaffold;
 };
 
-let out = o([1, 3, 4, 5, 9, 12, 30])
+let ltten = o([1, 3, 4, 5, 9, 12, 30])
     .a()
     .b(e => e < 10)
     .c()
     .d();
+console.log(ltten); // [ 1, 3, 4, 5, 9 ]
 
-let out = o([1, 3, 4, 5, 9, 12, 30])
+let doublethrees = o([1, 3, 4, 5, 9, 12, 30, 33, 47])
     .a(e => 2 * e)
-    .b(e => e % 3)
+    .b(e => e % 3 === 0)
     .c()
     .d();
+console.log(doublethrees); // [ 6, 18, 24, 60, 66 ]
 
-console.log(out);
+let summed = o([1, 2, 3])
+    .a(e => 3 * e)
+    .b(e => e % 3 === 0)
+    .c((p, c) => { return p + c; }, 0)
+    .d();
+console.log(summed);
